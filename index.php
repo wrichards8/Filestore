ba8ee0ac96f8aea0f49047a2ac1756b060fe2a9c
wrichards8 <wrichards@mediatwist.co.uk>
2013-09-06 06:25:23 +0100

    Committed new index
    This new update makes the script just one file

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
-<?php  require_once("mime.php");
+<?php if(file_exists("store")==FALSE)
+{
+	mkdir("store");
+	/*If the "store" folder does not exist then it will be created dynamically */
+}
+$folder = dirname(__FILE__)."/store"; 
+/* This sets the $folderscan variable to the current folder */
+
+$ignore_files = array(".", "..", "index.php", "main.css", ".htaccess"); 
+/* Since we shall flip the $folder, we need to tell it which values to unset, The easiest way I found was to use an array to store the values */
+
+$folder = scandir($folder);
+/* This defines the $folder variable which we use later */
+
+$folder = array_flip($folder);
+/* This flips the $folder array so we can unset the values */
+
+foreach ($ignore_files as $i)
+{
+	unset($folder[$i]);
+}
+
+$folder = array_flip($folder);
+/* We flipped the array earlier, so we need to flip it the right way */ ?>
 ?><!DOCTYPE html>
 <html lang="en-GB">
 <head>
@@ -7,8 +30,7 @@
 </head>
 <body>
 <?php echo "<h1><a href=\"#files\" tabindex=\"1\"><img src=\"skip.png\" alt=\"Skip to Folder Contents\"></a>\n";
-echo "File Storage Area\n";
-echo "<a href=\"#extra\" tabindex=\"2\"><img src=\"skip.png\" alt=\"Skip to More Information\"></a></h1>\n";
+echo "File Storage Area\n</h1>\n";
 echo "<div id=\"filebox\">\n";
 echo "<h2><a id=\"files\">Main File Listing</a></h2>\n";
 echo "<table>\n";
